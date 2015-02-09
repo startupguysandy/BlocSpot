ef9ca96b619585bdf453f4893a1da8f3c99ba54b
startupguysandy <andy@thestartupguys.co.uk>
2015-02-09 23:37:40 +0000

    Initial Commit

---
+++ BlocSpot/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocSpot
+//
+//  Created by Andy Bradbury on 09/02/2015.
+//  Copyright (c) 2015 Tin Bear Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlocSpot/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlocSpot
+//
+//  Created by Andy Bradbury on 09/02/2015.
+//  Copyright (c) 2015 Tin Bear Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
